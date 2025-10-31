75a448455ab5d21195b918c2f01f39afcc60750d
Artur Silva <arturdasilvadossantos.2021@gmail.com>
2025-10-31 11:11:58 -0300

    Melhorando verificações de input
    Adicionando aos poucos mais verificações de input (verificando se o tipo de arquivo adicionado é compatível, se é uma imagem ou se é um áudio), pois, para a música ser adicionada corretamente, o arquivo precisa ser necessáriamente uma imagem ou um áudio

--- js/adicionarMusica.js
+++ js/adicionarMusica.js
@@ -8,6 +8,9 @@ const inputNomeMusica = document.getElementById("nome-musica")
 const inputNomeArtista = document.getElementById("nome-artista")
 const inputCorFundo = document.getElementById("cor-fundo")
 const botaoEnviar = document.querySelector("form input[type='button']")
+const tiposPermitidosImagem = ["image/png", "image/jpeg", "image/webp", "image/svg+xml"]
+const tiposPermitidosAudio = ["audio/mpeg", "audio/aac", "audio/wav"]
+
 
 // Definindo funções
 function abrirFecharTelaAdicionarMusica() {
@@ -35,7 +38,22 @@ function criarNovaMusica() {
     alterarInformacoes()
 }
 
+function alterarTextoLabel(input, label) {
+    if (!(tiposPermitidosImagem).includes(input.files[0].type)) {
+        alert("Arquivo não permitido")
+        input.value = ""
+        return
+    }
+    document.getElementById(label).textContent = input.files[0].name
+}
+
 // Adicionando eventos
 botaoAdicionarMusica.addEventListener("click", abrirFecharTelaAdicionarMusica)
 botaoFecharTela.addEventListener("click", abrirFecharTelaAdicionarMusica)
 botaoEnviar.addEventListener("click", criarNovaMusica)
+inputArquivoAudio.addEventListener("change", () => {
+    alterarTextoLabel(inputArquivoAudio, "label2-arquivo-audio")
+})
+inputArquivoImagem.addEventListener("change", () => {
+    alterarTextoLabel(inputArquivoImagem, "label2-arquivo-imagem")
+})
