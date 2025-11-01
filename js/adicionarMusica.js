fcc72da59265ec4bddad2bdf10b0519e1ebf7e28
artursilvagit <arturdasilvadossantos.2021@gmail.com>
2025-11-01 00:59:57 -0300

    Update adicionarMusica.js

--- js/adicionarMusica.js
+++ js/adicionarMusica.js
@@ -8,6 +8,9 @@ const inputNomeMusica = document.getElementById("nome-musica")
 const inputNomeArtista = document.getElementById("nome-artista")
 const inputCorFundo = document.getElementById("cor-fundo")
 const botaoEnviar = document.querySelector("form input[type='button']")
+const tiposPermitidosImagem = ["image/png", "image/jpeg", "image/webp", "image/svg+xml"]
+const tiposPermitidosAudio = ["audio/mpeg", "audio/aac", "audio/wav"]
+
 
 // Definindo funções
 function abrirFecharTelaAdicionarMusica() {
@@ -35,7 +38,26 @@ function criarNovaMusica() {
     alterarInformacoes()
 }
 
+function alterarTextoLabel(input, label, array) {
+    if (!(array.includes(input.files[0].type))) {
+        alert("Tipo de arquivo não permitido.")
+        input.value = ""
+        return
+    }
+    if (input.files[0].name.length >= 20) {
+        document.getElementById(label).textContent = `${input.files[0].name.slice(0, 18)}...`
+        return
+    }
+    document.getElementById(label).textContent = input.files[0].name
+}
+
 // Adicionando eventos
 botaoAdicionarMusica.addEventListener("click", abrirFecharTelaAdicionarMusica)
 botaoFecharTela.addEventListener("click", abrirFecharTelaAdicionarMusica)
 botaoEnviar.addEventListener("click", criarNovaMusica)
+inputArquivoAudio.addEventListener("change", () => {
+    alterarTextoLabel(inputArquivoAudio, "label2-arquivo-audio", tiposPermitidosAudio)
+})
+inputArquivoImagem.addEventListener("change", () => {
+    alterarTextoLabel(inputArquivoImagem, "label2-arquivo-imagem", tiposPermitidosImagem)
+})
